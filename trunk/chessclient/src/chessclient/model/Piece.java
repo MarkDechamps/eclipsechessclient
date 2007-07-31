28de21c27e9aa642c3d9bf0f316039de76456069
Mark.Dechamps <Mark.Dechamps@80aab1f4-8f36-0410-b475-db4a1166e30e>
2007-07-31 19:39:57 +0000

    Added new Piece class Added functionality to Square to add and remove attackers

---
+++ trunk/chessclient/src/chessclient/model/Piece.java
@@ -0,0 +1,15 @@
+package chessclient.model;
+
+public class Piece {
+	private boolean isWhite;
+
+	public boolean isWhite() {
+		return isWhite;
+	}
+
+	public void setWhite(boolean isWhite) {
+		this.isWhite = isWhite;
+	}
+	
+	
+}
